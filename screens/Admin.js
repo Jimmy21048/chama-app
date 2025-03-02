09aac0a6b170a469f7ad5a6f861f983042ab4a6b
Jimmy Rubia <rubiajimmy3@gmail.com>
2025-03-02 23:44:46 +0300

    adds add member functionality

--- screens/Admin.js
+++ screens/Admin.js
@@ -1,13 +1,61 @@
-import { Platform, SafeAreaView, StyleSheet, Dimensions, Text, View, TouchableOpacity } from "react-native";
+import { Platform, SafeAreaView, StyleSheet, Dimensions, Text, View, TouchableOpacity, TextInput, Alert } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { MaterialIcons } from "@expo/vector-icons";
+import { useState } from "react";
+import axios from "axios";
 
 const { width, height } = Dimensions.get('screen')
 export default function Admin({ navigation, route }) {
     const { user, pwd } = route.params
+    const [data, setData] = useState({
+        username: ''
+    })
+    const[loading, setLoading] = useState(false)
+    const[message, setMessage] = useState('')
 
-    const handleSettings = () => {
-        
+    const handleAddMember = () => {
+                Alert.alert(
+                    'Add Member',
+                    "Confirm Action to Add Member",
+                    [
+                        {
+                            text: "Cancel",
+                            style: "cancel"
+                        },
+                        {
+                            text: "Add",
+                            onPress: () => {
+                                axios.post('http://192.168.0.120:3000/userExists', {username: data.username}, setLoading(true))
+                                .then(response => {
+                                    const exists = response.data.exists
+
+                                    if(exists) {
+                                        setMessage('User already exists')
+                                        setTimeout(() => {
+                                            setMessage('')
+                                        }, 5000)
+                                    } else {
+                                        axios.post('http://192.168.0.120:3000/addUser', {username: data.username})
+                                        .then(res => {
+                                            if(res.data.added) {
+                                                setMessage('Member Successfully Added')
+                                                setTimeout(() => {
+                                                    setMessage('')
+                                                }, 5000) 
+                                            }
+                                        })
+                                    }
+                                    setLoading(false)
+                                }).catch(err => {
+                                    setMessage('Upload Failed')
+                                    setTimeout(() => {
+                                        setMessage('')
+                                    }, 5000)
+                                })
+                            }
+                        }
+                    ]
+                )
     }
 
     return (
@@ -23,6 +71,16 @@ export default function Admin({ navigation, route }) {
                     <MaterialIcons name="settings" color={'black'} size={40} />
                 </TouchableOpacity>
             </View>
+            <View style = { styles.body }>
+                <Text>{message}</Text>
+                <View style = {styles.addMember}>
+                    <Text style = {{ fontSize: 18 }}>Add Member</Text>
+                    <TextInput style = { styles.input } placeholder="username..." value={data.username} onChangeText={(text) => setData({...data, username: text})} />
+                    <TouchableOpacity disabled = {loading ? true : false} onPress={handleAddMember} activeOpacity={0.2} style = { styles.btnChange }>
+                        { loading ? <Text>Checking...</Text>: <Text>Add Member</Text> }
+                    </TouchableOpacity>
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
@@ -33,10 +91,37 @@ const styles = StyleSheet.create({
         flex: 1
     },
     header: {
-        height: height * 0.1,
+        height: height * 0.09,
         paddingHorizontal: 10,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
+    },
+    body: {
+        borderWidth: 1,
+        borderTopColor: 'black',
+        height: height * 0.9,
+        padding: 10
+    },
+    addMember: {
+        borderWidth: 1,
+        borderColor: 'black',
+        padding: 10,
+        gap: 10,
+        borderRadius: 5
+    },
+    btnChange: {
+        width: 150,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        backgroundColor: '#00B9E8'
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 10,
+        width: 150
     }
 })
